ceb1ceb5b1323c99b43c2617f42bb2242317aad3
joemcdonough15 <joemcdonoughmusic@gmail.com>
2024-12-02 21:54:15 -0500

    more style and refactoring

--- frontend/src/components/OurMission/OurMission.tsx
+++ frontend/src/components/OurMission/OurMission.tsx
@@ -5,13 +5,16 @@ const OurMission = () => {
     <section className="our-mission-container">
       <img
         className="our-mission-img"
-        // src="https://local-wild-images.s3.us-east-1.amazonaws.com/whale.png"
-        src="/assets/images/whale.png"
+        src="https://local-wild-images.s3.us-east-1.amazonaws.com/whale.png"
         alt="whale"
       />
+
       <div className="our-mission-text">
-        <h3>Our Mission</h3>
-        <p>this is our mission</p>
+        <h3 className="our-mission-header">Our Mission</h3>
+        <p>
+          To forge a community of wildlife photographers dedicated to their
+          craft and eager to reveal the beauty that exists around us
+        </p>
       </div>
     </section>
   );
