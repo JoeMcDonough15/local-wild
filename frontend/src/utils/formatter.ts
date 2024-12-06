3ead2c1170061064b9fb803619591652106ae412
joemcdonough15 <joemcdonoughmusic@gmail.com>
2024-12-06 15:16:47 -0500

    get dates to show on input fields for create and edit posts

--- frontend/src/utils/formatter.ts
+++ frontend/src/utils/formatter.ts
@@ -5,3 +5,20 @@ export const formattedDate = (date: Date) => {
     return backAsDate.toDateString();
   }
 };
+
+export const dateAsString = (date: Date) => {
+  if (typeof date === "string") {
+    date = new Date(date);
+  }
+  let month: string | number = date.getMonth() + 1;
+  const year = date.getFullYear().toString();
+  let day: string | number = date.getDate();
+  if (day < 10) {
+    day = `0${day}`;
+  }
+  if (month < 10) {
+    month = `0${month}`;
+  }
+
+  return `${year}-${month}-${day}`;
+};
